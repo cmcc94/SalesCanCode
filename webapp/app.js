d06d4f731eabfc19c7d1e1e1181cefe4683d38d9
cmcc94 <149782728+cmcc94@users.noreply.github.com>
2024-05-06 08:47:36 +0200

    Connect to MongoDB

--- webapp/app.js
+++ webapp/app.js
@@ -1,6 +1,14 @@
 //Basic Framework from: https://snippets.cacher.io/snippet/79619822b802882f9dbf
 const express = require("express");
+const request = require("request");
+const mongoose = require("mongoose");
 const app = express();
+
+//When strict option is set to true , Mongoose will ensure that only the fields that are specified in your Schema will be saved in the database, and all other fields will not be saved
+mongoose.set('strictQuery', false);
+
+//Give the abillity to set a custom PORT if needed
+const PORT = process.env.PORT || 3000;
 
 //Tell the app to look for external content within public
 app.use(express.static("public"));
@@ -25,14 +33,39 @@ app.get("/pgprocess", function(req, res){
     res.render("pgprocess");
 });
 
+
+app.get("/pictures", function(req, res){
+    console.log("entering picture side");
+    request("https://http.cat/status/100", function(error, response, body){
+        if(error){
+            res.send(error);
+        }else{
+            res.send(body);
+        }
+    })
+
+})
+
+
 //The error Route has to be at the end or all websites will result in an error
 app.get("*", function(req, res){
     res.send("Error 404. This Route does not exist");
 });
 //End Routes
 
-//Start server
-app.listen(3000, function(){
-    console.log("NodeJS Web Application is now running on port 3000");
-});
+//Connect to MongoDB
+const start = async() => {
+    try{
+        await mongoose.connect("mongodb+srv://developer:01rjOZq2Ba3TbC3Z@cluster0.6iobce9.mongodb.net/");
+        //Listen on given Port
+        app.listen(PORT, function(){
+            console.log("NodeJS Web Application is now running on port 3000");
+        });
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+//Start the server
+start();
 //End server
